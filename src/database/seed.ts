4877def986352b916dd1070bad3edf8898dd4951
trinm-3423 <nguyen.minh.tri@sun-asterisk.com>
2024-06-04 10:39:29 +0700

    chore: refactor seed data

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { randomIntegerFromRange } from '../utils';
 import { Role } from '../constants/enum';
+import { faker } from '@faker-js/faker';
+import { tagsSeedData } from '../constants/index';
 
 const prisma = new PrismaClient();
 
@@ -57,11 +59,14 @@ async function seedUsers() {
   );
   usersData.push(admin);
 
-  for (let i = 2; i < 12; i++) {
+  for (let i = 2; i < 22; i++) {
     const user = await createUser(
       i,
       `test${i}@gmail.com`,
-      `test${i}`,
+      faker.internet.userName({
+        firstName: `test${i}`,
+        lastName: `${Math.floor(10000000 + Math.random() * 90000000)}`,
+      }),
       'Aa@123456',
       await randomIntegerFromRange(Role.MODERATOR, Role.USER),
     );
@@ -74,14 +79,14 @@ async function seedUsers() {
 async function seedQuestions() {
   const questionsData = [];
 
-  for (let i = 1; i < 11; i++) {
+  for (let i = 1; i < 31; i++) {
     const question = await prisma.questions.upsert({
       where: { id: i },
       update: {},
       create: {
-        title: `Title question ${i}`,
-        description: `Desc ${i}`,
-        user_id: i,
+        title: faker.lorem.sentence({ min: 5, max: 5 }),
+        description: faker.lorem.sentences({ min: 6, max: 6 }),
+        user_id: await randomIntegerFromRange(1, 21),
       },
     });
     questionsData.push(question);
@@ -92,18 +97,26 @@ async function seedQuestions() {
 
 async function seedAnswers() {
   const answersData = [];
+  let answerId = 1;
+  const questionCount = await prisma.questions.count();
 
-  for (let i = 1; i < 11; i++) {
-    const answer = await prisma.answers.upsert({
-      where: { id: i },
-      update: {},
-      create: {
-        content: `Answer ${i} for question`,
-        question_id: await randomIntegerFromRange(1, 3),
-        user_id: await randomIntegerFromRange(1, 3),
-      },
-    });
-    answersData.push(answer);
+  for (let questionId = 1; questionId <= questionCount; questionId++) {
+    const numberOfAnswers = faker.number.int({ min: 0, max: 5 });
+
+    for (let j = 0; j < numberOfAnswers; j++) {
+      const answer = await prisma.answers.upsert({
+        where: { id: answerId },
+        update: {},
+        create: {
+          id: answerId,
+          content: faker.lorem.sentences({ min: 6, max: 6 }),
+          question_id: questionId,
+          user_id: await randomIntegerFromRange(1, 21),
+        },
+      });
+      answersData.push(answer);
+      answerId++;
+    }
   }
 
   console.log(answersData);
@@ -112,13 +125,13 @@ async function seedAnswers() {
 async function seedTags() {
   const tagsData = [];
 
-  for (let i = 1; i < 11; i++) {
+  for (const tagData of tagsSeedData) {
     const tag = await prisma.tags.upsert({
-      where: { id: i },
+      where: { name: tagData.name },
       update: {},
       create: {
-        name: `Tag ${i}`,
-        description: `Tag ${i} is blablabla`,
+        name: tagData.name,
+        description: tagData.description,
       },
     });
     tagsData.push(tag);
@@ -129,25 +142,30 @@ async function seedTags() {
 
 async function seedQuestionsTags() {
   const questionsTagsData = [];
+  const questionIds = await prisma.questions.findMany({
+    select: { id: true },
+  });
 
-  for (let i = 1; i < 11; i++) {
-    const questionId = await randomIntegerFromRange(1, 3);
-    const tagId = await randomIntegerFromRange(1, 3);
+  for (let questionId = 1; questionId <= questionIds.length; questionId++) {
+    const numberOfTags = faker.number.int({ min: 1, max: 5 });
 
-    const questionTag = await prisma.questionsTags.upsert({
-      where: {
-        question_id_tag_id: {
-          question_id: questionId,
-          tag_id: tagId,
+    for (let j = 1; j <= numberOfTags; j++) {
+      const questionTag = await prisma.questionsTags.upsert({
+        where: {
+          question_id_tag_id: {
+            question_id: questionId,
+            tag_id: j,
+          },
         },
-      },
-      update: {},
-      create: {
-        question_id: questionId,
-        tag_id: tagId,
-      },
-    });
-    questionsTagsData.push(questionTag);
+        update: {},
+        create: {
+          question_id: questionId,
+          tag_id: j,
+        },
+      });
+
+      questionsTagsData.push(questionTag);
+    }
   }
 
   console.log(questionsTagsData);
